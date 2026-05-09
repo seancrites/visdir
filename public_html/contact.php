7b13a3fa295ac5539964e763dbd41238757b9a7f
Sean Crites <sean.crites@gmail.com>
2026-05-09 13:28:21 -0800

    chore(release): bump version to 1.4.0

--- public_html/contact.php
+++ public_html/contact.php
@@ -4,7 +4,7 @@
  * visdir Contact Form Handler
  *
  * @package     visdir
- * @version     @@PROJECT_VERSION@@
+ * @version     1.4.0
  * @author      Sean Crites
  * @license     GPL-3.0
  * @link        https://github.com/seancrites/visdir
